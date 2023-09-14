06eeccc65e7e9491fb43248d9d241c0c14bf3a9d
Natan <zerogamerptbr@gmail.com>
2023-09-13 22:23:59 -0300

    :sparkles:feat: start to receive files from the user

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res);
@@ -11,6 +16,12 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (req, res) => {
     return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+    const { file } = req;
+    console.log(file);
+    return res.send();
 });
 
 export { categoriesRoutes };
