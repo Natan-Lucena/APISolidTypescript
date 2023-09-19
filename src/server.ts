b3d2b123b53da5b481bc05e5dba5f4b59c977bae
Natan <zerogamerptbr@gmail.com>
2023-09-18 22:23:27 -0300

    :books:docs:starting the documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
