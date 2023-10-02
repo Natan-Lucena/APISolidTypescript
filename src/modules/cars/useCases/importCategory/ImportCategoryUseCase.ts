19fca05eb5dd60aafa60eff7b5687eb182c49fad
Natan <zerogamerptbr@gmail.com>
2023-10-02 00:14:05 -0300

    :construction:build:deleting the tpm files

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -27,6 +27,7 @@ class ImportCategoryUseCase {
                     categories.push({ name, description });
                 })
                 .on("end", () => {
+                    fs.promises.unlink(file.path);
                     resolve(categories);
                 })
                 .on("error", (err) => {
