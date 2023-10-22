6eed2484e91420822f395b32ef01859f76bbce6f
Natan <zerogamerptbr@gmail.com>
2023-10-22 13:53:58 -0300

    :recycle:refc: dependencyInjecation on CreateSpecficication

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-    constructor(
-        private createSpecificationUseCase: CreateSpecificationUseCase,
-    ) {}
-
-    handle(req: Request, res: Response): Response {
+    async handle(req: Request, res: Response): Promise<Response> {
         const { name, description } = req.body;
-        this.createSpecificationUseCase.execute({ name, description });
+        const createSpecificationUseCase = container.resolve(
+            CreateSpecificationUseCase,
+        );
+        await createSpecificationUseCase.execute({ name, description });
         return res.status(201).send();
     }
 }
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,12 +1,17 @@
+import { inject, injectable } from "tsyringe";
+
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
     name: string;
     description: string;
 }
-
+@injectable()
 class CreateSpecificationUseCase {
-    constructor(private specificationsRepository: ISpecificationsRepository) {}
+    constructor(
+        @inject("SpecificationRepository")
+        private specificationsRepository: ISpecificationsRepository,
+    ) {}
 
     execute({ name, description }: IRequest): void {
         const specificationAlreadyExists =
