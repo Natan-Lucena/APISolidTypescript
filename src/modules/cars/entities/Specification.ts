580a87001befe17651c4c92d2cdfe2594e78fbfc
Natan <zerogamerptbr@gmail.com>
2023-10-28 10:03:56 -0300

    :construction:feat: specification entity

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,15 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("specifications")
 class Specification {
+    @PrimaryColumn()
     id?: string;
+    @Column()
     name: string;
+    @Column()
     description: string;
+    @CreateDateColumn()
     create_at: Date;
 
     constructor() {
