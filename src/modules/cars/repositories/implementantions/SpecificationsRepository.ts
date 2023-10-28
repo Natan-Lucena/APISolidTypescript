0d359c43a37ea9e2734057d2a1bc0fe66f4be7d3
Natan <zerogamerptbr@gmail.com>
2023-10-28 10:05:06 -0300

    :recycle:refac:specification reposity refactor

--- src/modules/cars/repositories/implementantions/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementantions/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { Repository, getRepository } from "typeorm";
+
 import { Specification } from "../../entities/Specification";
 import {
     ICreateSpecificationDTO,
@@ -5,24 +7,20 @@ import {
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-    private specifications: Specification[];
+    private repository: Repository<Specification>;
 
     constructor() {
-        this.specifications = [];
+        this.repository = getRepository(Specification);
     }
-    create({ description, name }: ICreateSpecificationDTO): void {
-        const specification = new Specification();
-        Object.assign(specification, {
-            name,
-            description,
-            create_at: new Date(),
-        });
-        this.specifications.push(specification);
+    async create({
+        description,
+        name,
+    }: ICreateSpecificationDTO): Promise<void> {
+        const specification = this.repository.create({ description, name });
+        await this.repository.save(specification);
     }
-    findByName(name: string): Specification {
-        const specification = this.specifications.find(
-            (specification) => specification.name === name,
-        );
+    async findByName(name: string): Promise<Specification> {
+        const specification = this.repository.findOne({ name });
         return specification;
     }
 }
