5827add52e99a9a1869f580c5d98927b86aa4d1c
Natan <zerogamerptbr@gmail.com>
2023-12-09 10:15:53 -0300

    :sparkles:feat:repository method findById

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -4,6 +4,7 @@ import { User } from "../entities/User";
 interface IUserRepository {
     create(data: ICreateUserDTO): Promise<void>;
     findByEmail(email: string): Promise<User>;
+    findById(id: string): Promise<User>;
 }
 
 export { IUserRepository };
