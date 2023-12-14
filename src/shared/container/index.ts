e5902faccd1b7b86fe3db10db98c842ff25440c4
Natan <zerogamerptbr@gmail.com>
2023-12-14 01:54:17 -0300

    :truck:chore: refactoring container index

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,9 @@ import { container } from "tsyringe";
 
 import { UserRepository } from "../../modules/accounts/infra/typeorm/repositories/UserRepository";
 import { IUserRepository } from "../../modules/accounts/repositories/IUserRepository";
-import { ICategoryRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoryRepository } from "../../modules/cars/infra/typeorm/repositories/CategoryRepository";
 import { SpecificationsRepository } from "../../modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICategoryRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoryRepository>(
