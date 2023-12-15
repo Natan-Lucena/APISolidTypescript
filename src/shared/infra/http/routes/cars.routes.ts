19855a416d4e997049eab9367dce69906273f923
Natan <zerogamerptbr@gmail.com>
2023-12-15 13:29:29 -0300

    :sparkles: feat: all routes using the middlewares corretly

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarContoler = new CreateCarController();
 
-carsRoutes.post("/", createCarContoler.handle);
+carsRoutes.post(
+    "/",
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarContoler.handle,
+);
 
 export { carsRoutes };
