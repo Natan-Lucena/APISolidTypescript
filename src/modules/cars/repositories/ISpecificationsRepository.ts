d4fdd72a63bbd664528d5b3768e5021a51333091
Natan <zerogamerptbr@gmail.com>
2023-12-17 13:48:10 -0300

    :recycle: refac: refatoring specification repositories

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -6,8 +6,12 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationsRepository {
-    create({ description, name }: ICreateSpecificationDTO): Promise<void>;
+    create({
+        description,
+        name,
+    }: ICreateSpecificationDTO): Promise<Specification>;
     findByName(name: string): Promise<Specification>;
+    findByIds(ids: string[]): Promise<Specification[]>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
