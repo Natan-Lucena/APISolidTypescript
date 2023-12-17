5422dcac7a252ffdf5a1dd552a0f2aa5f637a5fa
Natan <zerogamerptbr@gmail.com>
2023-12-17 13:46:47 -0300

    :recycle: refac: refatoring car entity

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -3,12 +3,15 @@ import {
     CreateDateColumn,
     Entity,
     JoinColumn,
+    JoinTable,
+    ManyToMany,
     ManyToOne,
     PrimaryColumn,
 } from "typeorm";
 import { v4 as uuid } from "uuid";
 
 import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
@@ -31,6 +34,14 @@ class Car {
     @ManyToOne(() => Category)
     @JoinColumn({ name: "categoryId" })
     category: Category;
+    @ManyToMany(() => Specification)
+    @JoinTable({
+        name: "specificationsCars",
+        joinColumns: [{ name: "carId" }],
+        inverseJoinColumns: [{ name: "specificationId" }],
+    })
+    specifications: Specification[];
+
     @Column()
     categoryId: string;
     @CreateDateColumn()
