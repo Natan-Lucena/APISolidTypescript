1b192a50d82a4645fff9d9c501cda508398f1af8
Natan <zerogamerptbr@gmail.com>
2023-12-17 13:49:25 -0300

    :sparkles: feat: create car specification done

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "../../../../modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -9,6 +10,7 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvaibleCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
     "/",
@@ -18,5 +20,11 @@ carsRoutes.post(
 );
 
 carsRoutes.get("/available", listAvaibleCarsController.handle);
+carsRoutes.post(
+    "/specifications/:id",
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarSpecificationController.handle,
+);
 
 export { carsRoutes };
