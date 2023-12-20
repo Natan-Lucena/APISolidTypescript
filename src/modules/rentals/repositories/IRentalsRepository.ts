9182dc836b414ef8fdbc603a57432c7f0e105869
Natan <zerogamerptbr@gmail.com>
2023-12-20 00:58:57 -0300

    :sparkles: feat: create rental method

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,6 +1,8 @@
+import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
 
 interface IRentalsRepository {
+    create(data: ICreateRentalDTO): Promise<Rental>;
     findOpenRentalByCar(carId: string): Promise<Rental>;
     findOpenRentalByUser(userId: string): Promise<Rental>;
 }
