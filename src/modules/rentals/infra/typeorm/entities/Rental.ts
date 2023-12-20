44709a48195d46b7c48cd59bd060621573867998
Natan <zerogamerptbr@gmail.com>
2023-12-20 20:38:58 -0300

    :recycle: refac: refacotoring rental entity with typeorm

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,24 +1,39 @@
-import { Entity } from "typeorm";
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryColumn,
+    UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuid } from "uuid";
+
+import { Car } from "../../../../cars/infra/typeorm/entities/Car";
 
 @Entity("rentals")
 class Rental {
+    @PrimaryColumn()
     id: string;
 
+    @ManyToOne(() => Car)
+    @JoinColumn({ name: "carId" })
+    car: Car;
+    @Column()
     carId: string;
-
+    @Column()
     userId: string;
-
+    @Column()
     startDate: Date;
-
+    @Column()
     endDate: Date;
-
+    @Column()
     expectedReturnDate: Date;
-
+    @Column()
     total: number;
-
+    @CreateDateColumn()
     created_at: Date;
-
+    @UpdateDateColumn()
     updated_at: Date;
 
     constructor() {
