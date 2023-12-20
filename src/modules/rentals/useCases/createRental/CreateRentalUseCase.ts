d6826bf9da70cd1d0c6839361eb5cf61cf0a10d1
Natan <zerogamerptbr@gmail.com>
2023-12-20 02:28:57 -0300

    :sparkles: feat: validate date of rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,9 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import dayjs from "dayjs";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import utc from "dayjs/plugin/utc";
 import { injectable } from "tsyringe";
 
 import { AppError } from "../../../../shared/errors/AppError";
 import { Rental } from "../../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../../repositories/IRentalsRepository";
 
+dayjs.extend(utc);
 interface IRequest {
     userId: string;
     carId: string;
@@ -28,6 +33,19 @@ class CreateRentalUseCase {
             await this.rentalsRepository.findOpenRentalByUser(userId);
         if (rentalOpenToUser) {
             throw new AppError("User already have a open rental");
+        }
+        const minimumHours = 24;
+
+        const expectedReturnDateFormat = dayjs(expectedReturnDate)
+            .utc()
+            .local()
+            .format();
+
+        const dateNow = dayjs().utc().local().format();
+
+        const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+        if (compare < minimumHours) {
+            throw new AppError("Invalid return date");
         }
 
         const rental = await this.rentalsRepository.create({
